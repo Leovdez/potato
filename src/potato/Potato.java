ac7f7bac6f57b7e6ff17a763552070af3c079fe9
Leovdez <42093040+Leovdez@users.noreply.github.com>
2018-08-04 12:37:35 -0400

    Initial commit

---
+++ src/potato/Potato.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package potato;
+import java.util.Scanner;
+        
+/**
+ *
+ * @author dapcep
+ */
+public class Potato {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {  
+        System.out.println("this is my class");
+       Scanner in=new Scanner(System.in);
+       
+      String x;
+       x=in.nextLine();
+          System.out.println(x);
+        // TODO code application logic here
+    }
+    
+}
